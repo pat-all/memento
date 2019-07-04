1c64ca077e393a5c58b403240f88ecb07f3fdce4
pat-all <topalov.petr@gmail.com>
2019-07-04 20:44:33 +0300

    start script for defect lib

---
+++ defects.js
@@ -0,0 +1,18 @@
+var from = null;
+var to = null;
+
+if(field('От') = 'ТП'){
+  from = 'от ТП ' + field('от ТП')[0].field('Адрес');
+} else if(field('От') = 'ТК'){
+  from = 'от ТК ' + field('от ТК')[0].field('#ТК');
+} 
+
+if(field('До') = 'ТП'){
+  to = 'до ТП ' + field('до ТП')[0].field('Адрес');
+} else if(field('До') = 'ТК'){
+  to = 'до ТК ' + field('до ТК')[0].field('#ТК');
+} else if(field('До') = 'ж/д'){
+  to = 'до ж/д ' + field('до ж/д')[0].field('до ж/д');
+}
+
+from + " " + to
