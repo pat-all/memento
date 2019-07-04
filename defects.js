dd822d440543ee4fe377045328599ee63f5756c6
pat-all <topalov.petr@gmail.com>
2019-07-04 20:50:16 +0300

    fixed

--- defects.js
+++ defects.js
@@ -1,17 +1,17 @@
 var from = null;
 var to = null;
 
-if(field('От') = 'ТП'){
+if(field('От') === 'ТП'){
   from = 'от ТП ' + field('от ТП')[0].field('Адрес');
-} else if(field('От') = 'ТК'){
+} else if(field('От') === 'ТК'){
   from = 'от ТК ' + field('от ТК')[0].field('#ТК');
 } 
 
-if(field('До') = 'ТП'){
+if(field('До') === 'ТП'){
   to = 'до ТП ' + field('до ТП')[0].field('Адрес');
-} else if(field('До') = 'ТК'){
+} else if(field('До') === 'ТК'){
   to = 'до ТК ' + field('до ТК')[0].field('#ТК');
-} else if(field('До') = 'ж/д'){
+} else if(field('До') === 'ж/д'){
   to = 'до ж/д ' + field('до ж/д')[0].field('до ж/д');
 }
 
