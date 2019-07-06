174cd9ef887aefa7db034def775cd0573314a7ad
pat-all <topalov.petr@gmail.com>
2019-07-06 15:27:11 +0300

    fix 4

--- defects.js
+++ defects.js
@@ -17,20 +17,24 @@ switch(equipmentType){
     defect = from + ": " + equipmentMark;
     break;
   case "Трубопровод":
-    if(field('От') === 'ТП'){
+    if(field('От') === 'ТП' || field('От') === 'в ТП'){
       from = 'ТП ' + field('от ТП')[0].field('Адрес');
-    } else if(field('От') === 'ТК'){
+    } else if(field('От') === 'ТК' || field('От') === 'в ТК'){
       from = 'ТК ' + field('от ТК')[0].field('#ТК');
-    } 
-
-    if(field('До') === 'ТП'){
-      to = 'ТП ' + field('до ТП')[0].field('Адрес');
-    } else if(field('До') === 'ТК'){
-      to = 'ТК ' + field('до ТК')[0].field('#ТК');
-    } else if(field('До') === 'ж/д'){
-      to = 'ж/д ' + field('до ж/д')[0].field('до ж/д');
+    } else if(field('От' === "в ж/д"){
+      from = "ж/д " + field('в ж/д')[0].field('Адрес');
     }
-    defectiveEquipment = from + "-" + to;
+    if(field('От') === 'ТП' || field('От') === 'ТК'){
+      if(field('До') === 'ТП'){
+      to = ' - ТП ' + field('до ТП')[0].field('Адрес');
+      } else if(field('До') === 'ТК'){
+      to = ' - ТК ' + field('до ТК')[0].field('#ТК');
+      } else if(field('До') === 'ж/д'){
+      to = ' - ж/д ' + field('до ж/д')[0].field('до ж/д');
+      }
+    }
+    
+    defectiveEquipment = from + to;
     break;
   default:
 }
