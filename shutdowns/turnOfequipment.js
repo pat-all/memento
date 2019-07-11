f90d126afaad83fc612f6b4770eed2ce392638f0
Patrick Topalov <topalov.petr@gmail.com>
2019-07-11 13:47:01 +0300

    fix bugs

--- shutdowns/turnOfequipment.js
+++ shutdowns/turnOfequipment.js
@@ -1,24 +1,24 @@
 /**
  * формируем список оключенного оборудования и/или потребителей
  */
-var equpTypes = field("Оборудование");
+var equpTypes = entry().field("Оборудование");
 var adressTP = "";
-var system = "";
+var systema = "";
 var pumps = [];
 var customers = [];
 var result = "";
-var pumpLib = libByName("Насосы").entries();
+var pumpLib = libByName("Насосы");
 var customerLib = libByName("Потребители");
 
 if(field("ТП") !== null){
-    adtessTP = field("ТП")[0].field("Адрес");
+    adtessTP = entry().field("ТП")[0].field("Адрес");
 }
 
 function equipment(equpType){
     switch(equpType){
         case "Система":
-            system = "ГВС";
-            pumps = pumpLib.find(adressTP).find(system);
+            systema = "ГВС";
+            pumps = pumpLib.find(adressTP).filter(function(pump){pump.field("Группа") === "НЦ-ГВС"});
             for(var i = 0; i < pumps.length; i++){
                 if(i === 0){
                     result = result + pumps[0]
