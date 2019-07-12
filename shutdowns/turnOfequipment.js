1d182b409dad4a975a0ec7df9c11aa31aad81d2b
Patrick Topalov <topalov.petr@gmail.com>
2019-07-12 10:01:12 +0300

    added all systems

--- shutdowns/turnOfequipment.js
+++ shutdowns/turnOfequipment.js
@@ -3,12 +3,33 @@
  */
 var equpTypes = entry().field("Оборудование");
 var adressTP = "";
-var systema = "";
-var pumps = [];
-var customers = [];
+var system = "";
+var resultPumps = [];
+var resultCustomers = [];
 var result = "";
 var pumpLib = libByName("Насосы");
 var customerLib = libByName("Потребители");
+
+function turnedOffPumps(system){
+    var pumps = pumpLib.find(adressTP);            
+            for(var i = 0; i < pumps.length; i++){
+                if(pumps[i].field('Группа') === system){
+                    if(result.length === 0){
+                        result = result + pumps[i].field("Маркировка");
+                        resultPumps.push(pumps[i]);
+                    } else {
+                        result = result + ', ' + pumps[i].field("Маркировка");
+                        resultPumps.push(pumps[i]);
+                    }
+                }
+            }
+    return resultPumps;
+}
+
+function turnedOffCustomers() {
+    var customers = customerLib.find(adressTP);
+    resultCustomers = customers;
+}
 
 if(entry().field("ТП") !== null){
     adressTP = entry().field("ТП")[0].field("Адрес");
@@ -17,27 +38,44 @@ if(entry().field("ТП") !== null){
 function equipment(equpType){
     switch(equpType){
         case "Система":
-            systema = "ГВС";
-            pumps = pumpLib.find(adressTP);            
-            for(var i = 0; i < pumps.length; i++){
-                if(pumps[i].field('Система') === systema){
-                    if(i === 0){
-                        result = result + pumps[0]
-                    } else {
-                        result = result + ', ' + pumps[i];
-                    }
+            var systems = entry().field("Системы");
+
+            for(var i = 0; i < systems.length; i++){
+                system = systems[i];
+                switch (system) {
+                    case "ГВС":
+                        turnedOffPumps("НЦ-ГВС");
+                        turnedOffPumps("ПЦН");
+                        turnedOffCustomers();
+                        break;
+                    case "ХВС":
+                        turnedOffPumps("ПХН");
+                        turnedOffCustomers();
+                        break;
+                    case "ЦО Зависимая":
+                        turnedOffCustomers();
+                        break;
+                    case "ЦО Независимая":
+                        turnedOffPumps("НЦО");
+                        turnedOffPumps("НПО");
+                        turnedOffCustomers();
+                        break;
+                    default:
+                        break;
                 }
             }
             break;
         case "Насос":
+            turnedOffCustomers();
             break;
         case "Потребитель":
             break;
         default:
-
+            break;
     }
 }
 
 equipment(equpTypes[0]);
 
-entry().set("Отключенное", result);
+entry().set("Отключенное", "Отключено потебителей: " + resultCustomers.length);
+entry().set("Насосы", resultPumps);
