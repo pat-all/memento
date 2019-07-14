6b7a37f55f30d39f0a9c61ad892afa938dbff04b
Patrick Topalov <topalov.petr@gmail.com>
2019-07-14 10:46:56 +0300

    fixing

--- landscaping/description.js
+++ landscaping/description.js
@@ -4,9 +4,11 @@ var description = "";
 if (excavation.length > 0) {
   description = excavation[0].field("description");
 } else {
-  var from = field("от " + field("От"))[0].field("name");
-  var to = field("до " + field("До"))[0].field("name");
-  description = "От " + from + "-" + to;
+  var from = 'от ' + field("От");
+  var to = 'до ' + field("До");
+  var start = field(from)[0].field("name");
+  var end = field(to)[0].field("name");
+  description = "От " + start + "-" + end;
 }
 
 description
