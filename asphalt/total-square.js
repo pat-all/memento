54cd6027403539c4ea474add253b946b25e4bcd1
Patrick Topalov <topalov.petr@gmail.com>
2019-08-04 12:17:52 +0300

    fix total

--- asphalt/total-square.js
+++ asphalt/total-square.js
@@ -5,10 +5,10 @@ var totalSquare = 0;
 for (let index = 0; index < fragments.length; index++) {
   const fragment = fragments[index];
   if(fragment.field("Действие") === "Суммировать"){
-    totalSquare += fragment.field("S");
+    totalSquare = totalSquare + Number(fragment.field("S"));
   } else if (fragment.field("Действие") === "Отнять"){
-    totalSquare -= fragment.field("S");
+    totalSquare = totalSquare - Number(fragment.field("S"));
   }
 }
 
-totalSquare;
+totalSquare + amendment;
