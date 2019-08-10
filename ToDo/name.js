77793f94f33949e07ea389c84640916a1d62453d
Patrick Topalov <topalov.petr@gmail.com>
2019-08-10 13:26:08 +0300

    reworked todos name;

--- ToDo/name.js
+++ ToDo/name.js
@@ -1,5 +1,17 @@
-var name = field("Тип");
-if(name === "Другое"){
-  name = field("Другое");
+var type = field("Тип");
+var name = "";
+
+switch (type) {
+  case "Другое":
+    name = field("Другое");
+    break;
+  case "Дефекты": 
+    var defectsCount = field("Дефекты").length;
+    name = type + " [" + defectsCount + "]";
+    break;
+  default:
+    name = type;
+    break;
 }
-name
+
+name;
