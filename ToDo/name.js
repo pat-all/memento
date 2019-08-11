cb156c22aa9a8608a435bcfd67ce43a0402aebc8
Patrick Topalov <topalov.petr@gmail.com>
2019-08-11 16:41:55 +0300

    refactor

--- ToDo/name.js
+++ ToDo/name.js
@@ -3,19 +3,23 @@ var planed = field("Запланировать");
 var planDate = field("Запланировано");
 var name = "";
 
+function showCounter(counter){
+  return " [" + counter + "]";
+}
+
 switch (type) {
   case "Монтировать насосы":
   case "Насосы на склад":
   case "Демонтировать насосы":
     var pumpsCount = field("Насосы").length;
-    name = type + " [" + pumpsCount + "]";
+    name = type + showCounter(pumpsCount);
     break;
   case "Другое":
     name = field("Другое");
     break;
   case "Дефекты": 
     var defectsCount = field("Дефекты").length;
-    name = type + " [" + defectsCount + "]";
+    name = type + showCounter(defectsCount);
     break;
   case "Раскопка":
     var excavation = field("Раскопка")[0];
@@ -27,11 +31,11 @@ switch (type) {
     break;
   case "Отключения":
     var shutdownsCount = field("Отключения").length;
-    name = type + " [" + shutdownsCount + "]";
+    name = type + showCounter(shutdownsCount);
     break;
   case "Плановые ремонты":
       var planedRepairsCount = field("Ремонты").length;
-      name = type + " [" + planedRepairsCount + "]";    
+      name = type + showCounter(planedRepairsCount);    
     break;
   default:
     name = type;
